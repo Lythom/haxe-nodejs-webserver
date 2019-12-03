318c5855768b700e2edae87a15702c0f1a981ab5
Lythom <sam_1er2002@yahoo.fr>
2019-12-03 11:16:02 +0100

    Update README with debug instructions

--- Main.hx
+++ Main.hx
@@ -4,12 +4,29 @@ import js.npm.Express;
 import js.npm.express.BodyParser;
 import js.npm.express.Session;
 
-extern class RequestWithMiddlwares extends Request {
-	public var body:{username:String, password:String};
+extern class RequestWithSession extends Request {
 	public var session:{authenticated:Bool};
 }
 
+extern class RequestLogin extends RequestWithSession {
+	public var body:{username:String, password:String};
+}
+
+extern class RequestSubscribe extends RequestWithSession {
+	public var body:{username:String, password:String, email:String};
+}
+
+typedef User = {
+	var username:String;
+	var password:String;
+	var email:String;
+}
+
+// TODO: parler de la mémoire dans les programmes
+
 class Main {
+	static var users:Array<User> = new Array<User>();
+
 	static function main() {
 		var server:Express = new js.npm.Express();
 		server.use(BodyParser.json({limit: '5mb', type: 'application/json'}));
@@ -22,7 +39,7 @@ class Main {
 			res.end(Std.string(Math.random()));
 		});
 		server.post('/login', function(expressReq:Request, res:Response) {
-			var req:RequestWithMiddlwares = cast(expressReq);
+			var req:RequestLogin = cast(expressReq);
 			switch (req.body) {
 				case {username: "theuser", password: "thepassword"}:
 					req.session.authenticated = true;
@@ -33,20 +50,38 @@ class Main {
 					req.session.authenticated = false;
 					res.send(400, "Bad Request");
 				case {username: uname, password: pwd}:
-					// check uname and pwd in database
-					req.session.authenticated = false;
-					res.send(401, "Unauthorized");
+					if (userexists(uname, pwd)) {
+						req.session.authenticated = true;
+						res.send(200, "OK");
+					} else {
+						// check uname and pwd in database
+						req.session.authenticated = false;
+						res.send(401, "Unauthorized");
+					}
+			}
+		});
+		server.post('/subscribe', function(expressReq:Request, res:Response) {
+			var req:RequestSubscribe = cast(expressReq);
+			switch (req.body) {
+				case {username: username, password: password, email: email}
+					if (username == null || password == null || email == null):
+					// username and password and email must be provided
+					res.send(400, "Bad Request");
+				case {username: username, password: password, email: email}:
+					if (!userexists(username, password))
+						users.push({username: username, password: password, email: email});
+					res.send(200, "OK");
 			}
 		});
 
 		server.post('/logout', function(expressReq:Request, res:Response) {
-			var req:RequestWithMiddlwares = cast(expressReq);
+			var req:RequestWithSession = cast(expressReq);
 			req.session.authenticated = false;
 			res.send(200, "OK");
 			return;
 		});
 		server.get('/status', function(expressReq:Request, res:Response) {
-			var req:RequestWithMiddlwares = cast(expressReq);
+			var req:RequestWithSession = cast(expressReq);
 			trace(req.session.authenticated);
 			res.send(200, req.session.authenticated ? "Authentifié" : "Visiteur");
 		});
@@ -57,5 +92,9 @@ class Main {
 		}
 		server.listen(port, '127.0.0.1');
 		trace('Server running at http://127.0.0.1:${port}/');
+	}
+
+	static function userexists(uname:String, pwd:String):Bool {
+		return users.filter(u -> u.username == uname && u.password == pwd).length > 0;
 	}
 }
--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-Projet d'exemple API en haxe avec Express
+# Projet d'exemple API en haxe avec Express
+
+## Installation
 
 1. Installer haxe depuis https://haxe.org. (Version 4.0.2 testée)
 2. Installer NodeJS depuis https://nodejs.org/en/ (Version 10.17.0 testée)
@@ -10,4 +12,23 @@ haxe build.hxml
 npx nodemon main.js
 ```
 
+
+## Description
+
+Haxe est un langage pouvant être transpilé et exécuté dans de nombreux environnement. Nous utilisons ici haxe pour créer un fichier JavaScript qui sera exécuté par NodeJS. haxe utilisera des bibliothèque javascript existantes comme express. Pour conserver le typage fort dans les interactions entre notre application haxe/js et les bibliothèques JavaScript, des types "extern" sont déclarés. js-kit (bibliothèque haxe) permet de générer automatiquement un certain nombre d'extern, hxnodejs viens également avec des extern pré-configurés pour express.
+
+
+## Informations
+
 nodemon permet de redémarrer automatiquement le serveur node au changement de fichier.
+
+Une configuration de deboggage "Debug main.js" permet de débogger le serveur depuis vscode.
+
+
+## Configurer le débogueur VSCode pour une application NodeJS
+
+Pour configurer le débogueur depuis un projet vide :
+1. Dans build.hxml, ajouter l'instruction -debug pour générer les source-maps.
+2. Cliquer sur l'onglet Debug de vscode
+3. Créer une nouvelle configuration pour NodeJS
+4. Modifier la valeur de `program` dans la configuration de `launch.json` pour indiquer le chemin du fichier main.js: `"program": "${workspaceFolder}\\dist\\main.js"`
