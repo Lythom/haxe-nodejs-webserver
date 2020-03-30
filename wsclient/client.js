66d7f0956f271a688d345ba53d709acb3bdad7fc
Lythom <sam_1er2002@yahoo.fr>
2020-03-30 14:35:54 +0200

    + Build

--- wsclient/client.js
+++ wsclient/client.js
@@ -5,7 +5,7 @@
   var SERVER = 'backend.cleverapps.io';
   // var SERVER = 'localhost:1337';
   // var SERVER = window.location.host || 'localhost';
-  var PROTOCOL = 'https:';
+  var PROTOCOL = location.protocol;
   var WS_PROTOCOL = PROTOCOL === 'https:' ? 'wss:' : 'ws:';
 
   var ws = new WebSocket(`${WS_PROTOCOL}//${SERVER}/`);
